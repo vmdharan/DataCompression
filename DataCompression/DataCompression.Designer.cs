f9d9ab6ae0e825e4422dc1fa3cc878936736fc31
vmdharan <vmdharan@users.noreply.github.com>
2015-12-27 18:44:17 +1100

    Updated form to allow opening files
    - Added an 'openFileDialog' window.
    - Added a text box that displays the selected input file.
    - Added a label that displays the input file size.

--- DataCompression/DataCompression.Designer.cs
+++ DataCompression/DataCompression.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.inputTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openInputFile = new System.Windows.Forms.OpenFileDialog();
+            this.selectBtn = new System.Windows.Forms.Button();
+            this.labelFS = new System.Windows.Forms.Label();
+            this.labelFSbytes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputTB
+            // 
+            this.inputTB.Location = new System.Drawing.Point(59, 29);
+            this.inputTB.Name = "inputTB";
+            this.inputTB.ReadOnly = true;
+            this.inputTB.Size = new System.Drawing.Size(368, 20);
+            this.inputTB.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input file";
+            // 
+            // selectBtn
+            // 
+            this.selectBtn.Location = new System.Drawing.Point(352, 55);
+            this.selectBtn.Name = "selectBtn";
+            this.selectBtn.Size = new System.Drawing.Size(75, 23);
+            this.selectBtn.TabIndex = 2;
+            this.selectBtn.Text = "Browse...";
+            this.selectBtn.UseVisualStyleBackColor = true;
+            this.selectBtn.Click += new System.EventHandler(this.selectBtn_Click);
+            // 
+            // labelFS
+            // 
+            this.labelFS.AutoSize = true;
+            this.labelFS.Location = new System.Drawing.Point(56, 65);
+            this.labelFS.Name = "labelFS";
+            this.labelFS.Size = new System.Drawing.Size(89, 13);
+            this.labelFS.TabIndex = 3;
+            this.labelFS.Text = "File size (in bytes)";
+            // 
+            // labelFSbytes
+            // 
+            this.labelFSbytes.AutoSize = true;
+            this.labelFSbytes.Location = new System.Drawing.Point(187, 65);
+            this.labelFSbytes.Name = "labelFSbytes";
+            this.labelFSbytes.Size = new System.Drawing.Size(13, 13);
+            this.labelFSbytes.TabIndex = 4;
+            this.labelFSbytes.Text = "0";
+            // 
+            // DataCompression
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(437, 196);
+            this.Controls.Add(this.labelFSbytes);
+            this.Controls.Add(this.labelFS);
+            this.Controls.Add(this.selectBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputTB);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DataCompression";
+            this.Text = "Data Compression Utility";
+            this.Load += new System.EventHandler(this.DataCompression_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox inputTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openInputFile;
+        private System.Windows.Forms.Button selectBtn;
+        private System.Windows.Forms.Label labelFS;
+        private System.Windows.Forms.Label labelFSbytes;
     }
 }
 
--- DataCompression/DataCompression.cs
+++ DataCompression/DataCompression.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,35 @@ namespace DataCompression
         public DataCompression()
         {
             InitializeComponent();
+        }
+
+        private void DataCompression_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectBtn_Click(object sender, EventArgs e)
+        {
+            Stream stream1 = null;
+
+            if(openInputFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if((stream1 = openInputFile.OpenFile()) != null)
+                    {
+                        inputTB.Text = openInputFile.FileName;
+                        using (stream1)
+                        {
+                            labelFSbytes.Text = stream1.Length.ToString();
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Unable to read file from disk. Error details: " + ex.Message);
+                }
+            }
         }
     }
 }
